45779bccab86507c40dabaef3001da1232fe8df9
신희성 <vvwv3092@gmail.com>
2023-11-01 17:26:04 +0900

    init

---
+++ src/main/java/com/example/hellochat/HelloChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellochat/HelloChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
