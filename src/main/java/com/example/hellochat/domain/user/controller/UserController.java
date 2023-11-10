7f2394177e155eb26a7eda5bc4d0b4696ba54286
신희성 <vvwv3092@gmail.com>
2023-11-10 23:47:25 +0900

    Add UserInfo API service

--- src/main/java/com/example/hellochat/domain/user/controller/UserController.java
+++ src/main/java/com/example/hellochat/domain/user/controller/UserController.java
@@ -1,21 +1,48 @@
 package com.example.hellochat.domain.user.controller;
 
-import com.example.hellochat.domain.user.dto.response.UserInfoResponse;
-import com.example.hellochat.global.security.UserDetailsImpl;
+import com.example.hellochat.domain.user.service.UserService;
+import com.example.hellochat.domain.user.dto.request.LoginRequest;
+import com.example.hellochat.domain.user.dto.request.SignupRequest;
+import com.example.hellochat.domain.user.entity.UserEntity;
+import com.example.hellochat.global.jwt.JwtUtil;
+import com.example.hellochat.global.jwt.service.RefreshTokenService;
+import com.example.hellochat.global.util.MsgResponseDto;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/auth")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<UserInfoResponse> userfind(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public static final String BEARER_PREFIX = "Bearer ";
 
+    private final UserService userService;
+    private final RefreshTokenService refreshTokenService;
+
+    // 회원 가입
+    @PostMapping("/signup")
+    public ResponseEntity<MsgResponseDto> signup(@RequestBody @Valid SignupRequest request) {
+        userService.signup(request);
+        return ResponseEntity.ok(new MsgResponseDto("회원가입 완료", HttpStatus.CREATED.value()));
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity<MsgResponseDto> login(@RequestBody LoginRequest loginRequestDto, HttpServletResponse response) {
+        UserEntity user = userService.login(loginRequestDto);
+
+        String refreshToken = JwtUtil.createRefreshToken(user.getName());
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, BEARER_PREFIX + JwtUtil.createToken(user.getName(), user.getRole()));
+        response.addHeader("Refresh-Token", BEARER_PREFIX + refreshToken);
+
+        refreshTokenService.saveRefreshToken(refreshToken, user.getUsersId());
+
+        return ResponseEntity.ok(new MsgResponseDto("로그인 완료", HttpStatus.OK.value()));
     }
 }
---
+++ src/main/java/com/example/hellochat/domain/user/service/UserInfoService.java
@@ -0,0 +1,35 @@
+package com.example.hellochat.domain.user.service;
+
+import com.example.hellochat.domain.user.dto.response.UserInfoResponse;
+import com.example.hellochat.domain.user.entity.UserEntity;
+import com.example.hellochat.domain.user.repository.UserRepository;
+import com.example.hellochat.global.exception.CustomException;
+import static com.example.hellochat.global.exception.ErrorCode.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class UserInfoService {
+    private final UserRepository userRepository;
+
+    public UserInfoResponse findUser(Long userId, UserEntity user) {
+        // 추가 정보 불러오는 부분 추가. 팔로우, 팔로잉, 게시글
+
+        UserEntity findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(NOT_MATCH_INFORMATION));
+
+        return UserInfoResponse.builder()
+                .id(findUser.getUsersId())
+                .username(findUser.getName())
+                .explain(findUser.getExplain())
+                .followers(0L) // temp
+                .following(0L) // temp
+                .isFollowed(Objects.equals(findUser.getUsersId(), user.getUsersId()) ? null : false) // temp
+                .posts(List.of()) // temp
+                .build();
+    }
+}
