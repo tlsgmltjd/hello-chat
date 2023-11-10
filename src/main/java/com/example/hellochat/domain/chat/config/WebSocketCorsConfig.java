01942f5b5a3e313ae0f4eabc6a80610883b7f49b
신희성 <vvwv3092@gmail.com>
2023-11-10 19:27:54 +0900

    Add cors response header

--- src/main/java/com/example/hellochat/domain/chat/config/WebSocketCorsConfig.java
+++ src/main/java/com/example/hellochat/domain/chat/config/WebSocketCorsConfig.java
@@ -11,6 +11,7 @@ public class WebSocketCorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
+                .exposedHeaders("Authorization", "Refresh-Token") // response header 추가
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
