681cc3a8870a8de76db206f319c8e9785d288910
신희성 <vvwv3092@gmail.com>
2023-11-12 03:19:32 +0900

    Add boardFind controller

--- src/main/java/com/example/hellochat/domain/Board/controller/BoardController.java
+++ src/main/java/com/example/hellochat/domain/Board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.hellochat.domain.Board.controller;
 
 import com.example.hellochat.domain.Board.dto.request.CreateBoardRequest;
+import com.example.hellochat.domain.Board.dto.response.BoardsResponse;
 import com.example.hellochat.domain.Board.service.BoardService;
 import com.example.hellochat.global.security.UserDetailsImpl;
 import com.example.hellochat.global.util.MsgResponseDto;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +23,10 @@ public class BoardController {
     public ResponseEntity<MsgResponseDto> boardPost(@RequestBody CreateBoardRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         boardService.postBoard(request, userDetails.getUser());
         return ResponseEntity.ok(new MsgResponseDto("게시글 등록이 완료되었습니다.", HttpStatus.OK.value()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BoardsResponse>> boardsFind() {
+        return ResponseEntity.ok(boardService.findBoad());
     }
 }
